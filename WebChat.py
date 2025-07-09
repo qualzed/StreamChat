dd90d1c5babb9f9a512127880d4f8e495493db55
unknown <klimpodogov365@gmail.com>
2025-07-09 15:41:34 +0300

    Changes to be committed: 	modified:   WebChat.py

--- WebChat.py
+++ WebChat.py
@@ -3,11 +3,12 @@ import webview
 supported_links = ['youtube', 'twitch']
 
 def check_link(chat_link):
-    if "twitch" in chat_link or "youtube" in chat_link:
-        webview.create_window("StreamChat", chat_link)
-        webview.start()
-    else:
-        print("Link not supported")
+    for links in supported_links:
+        if(links in chat_link):
+            webview.create_window("StreamChat", chat_link)
+            webview.start()
+        else:
+            print("Link not supported")
 
-chat_link = input("Link on chat: ")
+chat_link = str(input("Link on chat: "))
 check_link(chat_link)
